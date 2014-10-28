cbaf2d95b889491e1f43ee9e083ca051170d2563
Max <Max@10.50.240.179>
2014-10-28 18:59:56 +0100

    [URL Management] Entity Model implemented

---
+++ src/java/cat/urv/deim/sob/db/UrlDAO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cat.urv.deim.sob.db;
+
+import cat.urv.deim.sob.exceptions.SOBException;
+import cat.urv.deim.sob.models.SOBUrl;
+
+/**
+ *
+ * @author Max
+ */
+public interface UrlDAO {
+    
+    public SOBUrl get (String id) throws SOBException;
+    
+    public void add (SOBUrl url) throws SOBException;
+       
+}
